082cb5772546e3fcad67845fd1aafad86ac829f5
Karen Kvam <karen.kvam@ymail.com>
2022-02-14 20:50:17 -0200

    first commit

---
+++ javascript_note_api/config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/javascriptNote', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+}
+).then(() => console.log('connection succesful'))
+  .catch((err) => console.error(err));
